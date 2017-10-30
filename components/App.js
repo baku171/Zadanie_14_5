7b6bf49af00e3705f966f81e6daed6afb68a6477
baku171 <pbak@esri.pl>
2017-10-30 14:53:54 +0100

    Zadanie 14.5

--- components/App.js
+++ components/App.js
@@ -28,8 +28,8 @@ var App = React.createClass({
     render: function() {
         return (
             React.createElement('div', { className: 'app' },
-                React.createElement(ContactForm, { contact: ContactForm }),
-                React.createElement(Contacts, { item: contacts }, {})
+                React.createElement(ContactForm, { contact: contactForm }),
+                React.createElement(Contacts, { items: contacts }, {})
             )
         );
     }
